ebd7761a0b338b4218c33e0eb05a6bc30c454d63
Ben Hayes <b.t.hayes@comcast.net>
2021-07-14 17:45:56 -0500

    commit tree d22449541d7de4a3dfcc4ff9528be8281f4896bc

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+function map(sourceArray, fn) {
+  let newArray = [];
+  sourceArray.forEach(element => newArray.push(fn(element)));
+  return newArray;
+}
+
+function reduce(sourceArray, fn, startingPoint) {
+  let value;
+  if (startingPoint) {
+    value = startingPoint;
+    for (const element of sourceArray) {
+      value = fn(element, value);
+    }
+    return value;
+  } else {
+    value = sourceArray[0];
+    for (let i = 1; i < sourceArray.length; i++) {
+      value = fn(sourceArray[i], value);
+    }
+    return value;
+  }
+}
